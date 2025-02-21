368b15dadbe463106e23c99d436aecbddd7013a6
Rishabh2457 <rishabh.kushwah_cs.aiml21@gla.ac.in>
2025-02-21 20:32:09 +0530

    Rishabh add: Sol of exception handling due date 21 feb

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string filePath = "data.txt";
+
+        try
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                Console.WriteLine("Data");
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("file not found");
+        }
+
+        catch (IOException ex)
+        {
+            Console.WriteLine("An IO error acccured " + ex.Message);
+        }
+    }
+}
---
+++ Class3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+
+public class InvalidAgeException : Exception
+{
+    public InvalidAgeException(string message) : base(message)
+    {
+
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter the user's age: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            ValidAge(num);
+        }
+        catch (InvalidAgeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        catch (FormatException)
+        {
+            Console.WriteLine("Enter numeric value");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An unexpected error is occurred: " + ex.Message);
+        }
+
+    }
+    public static void ValidAge(int age)
+    {
+
+
+
+        if (age >= 18)
+        {
+            Console.WriteLine("Access granted");
+        }
+        else if (age < 18)
+        {
+            throw new InvalidAgeException("Age must be 18 or above");
+        }
+
+
+
+    }
+
+}
