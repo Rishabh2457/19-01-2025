3fe9b8db57e3926f0e47d9770b2374f8acf728d4
Rishabh2457 <rishabh.kushwah_cs.aiml21@gla.ac.in>
2025-02-28 23:10:27 +0530

    Rishabh Add: Submission of web api due date 28 feb

---
+++ BusinessLayer/Services/UserRegistrationBL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using RepositoryLayer.Services;
+using ModelLayer.DTO;
+//using RepositoryLayer.Service;
+
+namespace BusinessLayer.Services
+{
+    public class UserRegistrationBL
+    {
+        UserRegistrationRL _userRegistrationRL;
+        public UserRegistrationBL(UserRegistrationRL userRegistrationRL)
+        {
+            _userRegistrationRL = userRegistrationRL;
+        }
+        public List<User> RegistrationBL(User user)
+        {
+            //Check email of the user ,if user exist in list then we do not add it 
+            List<User> currentUsers = _userRegistrationRL.GetUsers();
+            foreach (User data in currentUsers)
+            {
+                if (data.Email.Equals(user.Email))
+                {
+                    return currentUsers;
+                }
+
+            }
+            //send it to repository layer to add the user
+            return _userRegistrationRL.RegistrationRL(user);
+
+
+
+
+        }
+        
+    }
+}
+
---
+++ Registration/Controllers/UserRegistration.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using BusinessLayer.Services;
+using RepositoryLayer.Services;
+using ModelLayer.DTO;
+
+namespace UserRegistration.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserRegistration : ControllerBase
+{
+    UserRegistrationBL _userRegistrationBL;
+    ResponseBody<string> response;
+    public UserRegistration(UserRegistrationBL userRegistrationBL)
+    {
+        _userRegistrationBL = userRegistrationBL;
+
+    }
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello!\n Register using Post Request...";
+    }
+
+    [HttpPost]
+    public List<User> Registration(User user)
+    {
+        List<User> result = _userRegistrationBL.RegistrationBL(user);
+
+        return result;
+
+
+    }
+
+}
